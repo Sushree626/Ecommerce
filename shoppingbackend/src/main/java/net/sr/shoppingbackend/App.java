a10fa22f56b43239423751d7afd576389ee4499b
SURAJ ROUT <SURAJ ROUT@LAPTOP-97MM4NES>
2020-09-28 19:04:18 +0530

    Successfully loaded Static Resources

---
+++ shoppingbackend/src/main/java/net/sr/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.sr.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
